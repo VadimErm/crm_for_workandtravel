0a79e09dc489aa48ae2353804542e1490e23a998
Stanislav <stanislav.freelancer2014@gmail.com>
2016-10-24 12:05:50 +0300

    First commit

---
+++ frontend/assets/CustomAsset.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stanislav
+ * Date: 24.10.16
+ * Time: 10:37
+ */
---
+++ frontend/views/layouts/gentelella.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stanislav
+ * Date: 20.10.16
+ * Time: 13:48
+ */
