032e4f96c81e660c9b59f1f5995aa5646fe8a7de
Stanislav <stanislav.freelancer2014@gmail.com>
2016-10-25 18:45:05 +0300

    Files folder

---
+++ files/config/main.php
@@ -0,0 +1,51 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-files',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'files\controllers',
+    'bootstrap' => ['log'],
+    'defaultRoute' => 'file',
+    'modules' => [],
+    'components' => [
+        'request' => [
+            'csrfParam' => '_csrf-files',
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+            'identityCookie' => ['name' => '_identity-files', 'httpOnly' => true],
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the backend
+            'name' => 'advanced-files',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        /*
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+        */
+    ],
+    'params' => $params,
+];
---
+++ files/controllers/FileController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace files\controllers;
+
+use yii\web\Controller;
+
+class FileController extends Controller
+{
+    public function actionIndex()
+    {
+        return 'File controller';
+    }
+}
