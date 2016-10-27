390b7377b6b248698e27a2cdc2a4bb2be97130b2
Stanislav <stanislav.freelancer2014@gmail.com>
2016-10-27 12:15:21 +0300

    Rbac roles migration

---
+++ console/migrations/m161027_085752_rbac_roles.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\db\Migration;
+
+class m161027_085752_rbac_roles extends Migration
+{
+    private $_auth;
+
+    public function __construct(array $config = [])
+    {
+        parent::__construct($config);
+    }
+
+    public function up()
+    {
+        $auth = Yii::$app->authManager;
+        
+        $studentRole = $auth->createRole('student');
+        $auth->add($studentRole);
+
+        $managerRole = $auth->createRole('manager');
+        $auth->add($managerRole);
+
+        $mainManagerRole = $auth->createRole('main_manager');
+        $auth->add($mainManagerRole);
+
+        $directorRole = $auth->createRole('director');
+        $auth->add($directorRole);
+    }
+
+    public function down()
+    {
+        $auth = Yii::$app->authManager;
+
+        $auth->removeAllRoles();
+    }
+}
