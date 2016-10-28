29dbd0541bc33f9727faf9f8ed78e9c7a6d3cc6f
Stanislav <stanislav.freelancer2014@gmail.com>
2016-10-28 10:59:55 +0300

    Add data table responsive

--- backend/views/student/index.php
+++ backend/views/student/index.php
@@ -25,21 +25,6 @@
                 </p>
                 <div id="datatable-responsive_wrapper" class="dataTables_wrapper form-inline dt-bootstrap no-footer">
                     <div class="row">
-                        <div class="col-sm-6">
-                            <div class="dataTables_length" id="datatable-responsive_length"><label>Показать <select
-                                        name="datatable-responsive_length" aria-controls="datatable-responsive"
-                                        class="form-control input-sm">
-                                        <option value="10">10</option>
-                                        <option value="25">25</option>
-                                        <option value="50">50</option>
-                                        <option value="100">100</option>
-                                    </select>записей</label></div>
-                        </div>
-                        <div class="col-sm-6">
-                            <div id="datatable-responsive_filter" class="dataTables_filter"><label>Поиск:<input
-                                        type="search" class="form-control input-sm" placeholder=""
-                                        aria-controls="datatable-responsive"></label></div>
-                        </div>
                     </div>
                     <div class="row">
                         <div class="col-sm-12">
@@ -167,75 +152,11 @@
                                     <td>6222</td>
                                     <td style="display: none;">b.nash@datatables.net</td>
                                 </tr>
-                                <tr role="row" class="even">
-                                    <td class="sorting_1" tabindex="0">Caesar</td>
-                                    <td>Vance</td>
-                                    <td>Pre-Sales Support</td>
-                                    <td>New York</td>
-                                    <td>21</td>
-                                    <td>2011/12/12</td>
-                                    <td>$106,450</td>
-                                    <td>8330</td>
-                                    <td style="display: none;">c.vance@datatables.net</td>
-                                </tr>
-                                <tr role="row" class="odd">
-                                    <td class="sorting_1" tabindex="0">Cara</td>
-                                    <td>Stevens</td>
-                                    <td>Sales Assistant</td>
-                                    <td>New York</td>
-                                    <td>46</td>
-                                    <td>2011/12/06</td>
-                                    <td>$145,600</td>
-                                    <td>3990</td>
-                                    <td style="display: none;">c.stevens@datatables.net</td>
-                                </tr>
-                                <tr role="row" class="even">
-                                    <td tabindex="0" class="sorting_1">Cedric</td>
-                                    <td>Kelly</td>
-                                    <td>Senior Javascript Developer</td>
-                                    <td>Edinburgh</td>
-                                    <td>22</td>
-                                    <td>2012/03/29</td>
-                                    <td>$433,060</td>
-                                    <td>6224</td>
-                                    <td style="display: none;">c.kelly@datatables.net</td>
-                                </tr>
                                 </tbody>
                             </table>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-sm-5">
-                            <div class="dataTables_info" id="datatable-responsive_info" role="status"
-                                 aria-live="polite">Showing 1 to 10 of 57 entries
-                            </div>
-                        </div>
-                        <div class="col-sm-7">
-                            <div class="dataTables_paginate paging_simple_numbers" id="datatable-responsive_paginate">
-                                <ul class="pagination">
-                                    <li class="paginate_button previous disabled" id="datatable-responsive_previous"><a
-                                            href="#" aria-controls="datatable-responsive" data-dt-idx="0" tabindex="0">Previous</a>
-                                    </li>
-                                    <li class="paginate_button active"><a href="#" aria-controls="datatable-responsive"
-                                                                          data-dt-idx="1" tabindex="0">1</a></li>
-                                    <li class="paginate_button "><a href="#" aria-controls="datatable-responsive"
-                                                                    data-dt-idx="2" tabindex="0">2</a></li>
-                                    <li class="paginate_button "><a href="#" aria-controls="datatable-responsive"
-                                                                    data-dt-idx="3" tabindex="0">3</a></li>
-                                    <li class="paginate_button "><a href="#" aria-controls="datatable-responsive"
-                                                                    data-dt-idx="4" tabindex="0">4</a></li>
-                                    <li class="paginate_button "><a href="#" aria-controls="datatable-responsive"
-                                                                    data-dt-idx="5" tabindex="0">5</a></li>
-                                    <li class="paginate_button "><a href="#" aria-controls="datatable-responsive"
-                                                                    data-dt-idx="6" tabindex="0">6</a></li>
-                                    <li class="paginate_button next" id="datatable-responsive_next"><a href="#"
-                                                                                                       aria-controls="datatable-responsive"
-                                                                                                       data-dt-idx="7"
-                                                                                                       tabindex="0">Next</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
                     </div>
                 </div>
 
